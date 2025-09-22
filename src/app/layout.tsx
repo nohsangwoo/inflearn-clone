48314e7748952338e58568d3107ea34729ed2578
nohsangwoo <nsgr12@naver.com>
2025-09-23 01:10:27 +0900

    fix: 확대방지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default async function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="링구스트" />
