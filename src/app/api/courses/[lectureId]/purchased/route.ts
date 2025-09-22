c86ffacecaf3ca131508b49d658fa7a48c2444ea
nohsangwoo <nsgr12@naver.com>
2025-09-23 01:29:59 +0900

    feat: lingoost 서버

--- src/app/api/courses/[lectureId]/purchased/route.ts
+++ src/app/api/courses/[lectureId]/purchased/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/lib/prismaClient"
 import { getAuthUserFromRequest } from "@/lib/auth/get-auth-user"
 
-export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params
+export async function GET(req: NextRequest, { params }: { params: Promise<{ lectureId: string }> }) {
+  const { lectureId: id } = await params
   console.log("params: ",id)
   const lectureId = Number(id)
   if (!Number.isFinite(lectureId)) return NextResponse.json({ purchased: false }, { status: 200 })
